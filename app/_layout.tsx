2805623d54677c73f1e546e61d9b13880c1c04a8
Sukhoruchko Oleh <osukhoruchko@gmail.com>
2025-07-26 12:02:35 +0300

    feat: Refactor tsconfig.json formatting, update RootLayout to use TamaguiProvider, and add Tamagui dependencies in package.json.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,13 @@
+import { defaultConfig } from "@tamagui/config/v4";
 import { Stack } from "expo-router";
+import { createTamagui, TamaguiProvider } from "tamagui";
+
+const config = createTamagui(defaultConfig);
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <TamaguiProvider config={config}>
+      <Stack />
+    </TamaguiProvider>
+  );
 }
